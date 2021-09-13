49c1f4d43e2572c3b422d22757940b04c112efe7
Chenlu Wang <46428070@qq.com>
2021-09-13 22:20:49 +1000

    updated solution for FirstBadVersion

--- src/com/simple/firstBadVersion/FirstBadVersion.java
+++ src/com/simple/firstBadVersion/FirstBadVersion.java
@@ -4,25 +4,16 @@ public class FirstBadVersion {
     public int firstBadVersion(int n) {
         int left = 1;
         int right = n;
-        int pivot = 0;
-        while (left <= right) {
-            pivot = left + (right - left) / 2;
+        while (left < right) {
+            int pivot = left + (right - left) / 2;
 
-            if (pivot == left || pivot == right) {
-                if (isBadVersion(pivot)) return pivot;
-                else return pivot + 1;
-            }
-
-            System.out.println("pivot： " + pivot);
             if (isBadVersion(pivot)) {
                 right = pivot;
-                System.out.println("right： " + right);
             } else {
-                left = pivot;
-                System.out.println("left： " + left);
+                left = pivot + 1;
             }
         }
-        return pivot;
+        return left;
     }
 
     private boolean isBadVersion(int pivot) {
