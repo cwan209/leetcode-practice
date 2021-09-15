42a669a2004aa54a7b575bd3327cb8f0f5dbb529
Chenlu Wang <46428070@qq.com>
2021-09-15 22:54:18 +1000

    rotate array approach 2

--- src/com/simple/rotateArray/RotateArray.java
+++ src/com/simple/rotateArray/RotateArray.java
@@ -5,29 +5,21 @@ import java.util.Arrays;
 public class RotateArray {
     public void rotate(int[] nums, int k) {
 
-        for (int i = 0; i < k; i++) {
-            int lastNumber = nums[nums.length - 1];
-            int nextNumber = nums[0];
-            int overwrittenNumber;
+        int n = nums.length;
+        int overwrittenNumberIndex;
 
-            for (int j = 0; j < nums.length; j++) {
+        int[] tempArray = Arrays.copyOf(nums, nums.length);
 
-                System.out.println(Arrays.toString(nums));
-
-                if (j == nums.length - 1) {
-                    nums[0] = lastNumber;
-                } else {
-                    overwrittenNumber = nums[j + 1];
-                    nums[j + 1] = nextNumber;
-                    nextNumber = overwrittenNumber;
-                }
-            }
-
-            System.out.println("==========================");
+        for (int j = 0; j < nums.length; j++) {
+            overwrittenNumberIndex = j + k > n - 1 ? j + k - n : j + k;
+            nums[overwrittenNumberIndex] = tempArray[j];
             System.out.println(Arrays.toString(nums));
-            System.out.println("==========================");
-
         }
+
+        System.out.println("==========================");
+        System.out.println(Arrays.toString(nums));
+        System.out.println("==========================");
+
 
     }
 }
