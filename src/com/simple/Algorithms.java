1299df16f967b73f471047117e1f415e72b24408
Chenlu Wang <46428070@qq.com>
2021-09-16 22:36:35 +1000

    reverse string

--- src/com/simple/Algorithms.java
+++ src/com/simple/Algorithms.java
@@ -1,14 +1,33 @@
 package com.simple;
 
 public class Algorithms {
-    public int[] twoSum(int[] numbers, int target) {
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] + numbers[j] == target) {
-                    return new int[] {i + 1, j + 1};
-                }
+    public int[] twoSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (target == sum) return  new int[]{left + 1, right + 1};
+            if (target > sum) {
+                left++;
+            } else {
+                right--;
             }
         }
+
         return null;
+    }
+
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char leftChar = s[left];
+            char rightChar = s[right];
+
+            s[left++] = rightChar;
+            s[right--] = leftChar;
+        }
     }
 }
--- test/com.simple/AlgorithmsTests.java
+++ test/com.simple/AlgorithmsTests.java
@@ -49,4 +49,32 @@ public class AlgorithmsTests {
         System.out.println(Arrays.toString(result));
         Assertions.assertArrayEquals(output, result);
     }
+    /**
+     Write a function that reverses a string. The input string is given as an array of characters s.
+     Example 1:
+
+     Input: s = ["h","e","l","l","o"]
+     Output: ["o","l","l","e","h"]
+     Example 2:
+
+     Input: s = ["H","a","n","n","a","h"]
+     Output: ["h","a","n","n","a","H"]
+
+
+     Constraints:
+
+     1 <= s.length <= 105
+     s[i] is a printable ascii character.
+     ***/
+    @Test void reverseString() {
+        // given
+        Algorithms algorithms = new Algorithms();
+        char[] hello = new char[]{'h', 'e', 'l', 'l', 'o'};
+
+        // when
+        algorithms.reverseString(hello);
+
+        // then
+        Assertions.assertArrayEquals(new char[]{'o', 'l', 'l', 'e', 'h'}, hello);
+    }
 }
