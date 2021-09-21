92909736e00da7d973cec9ea9e66e60531223f1b
Chenlu Wang <46428070@qq.com>
2021-09-21 21:59:27 +1000

    flood fill“

--- src/com/simple/Algorithms.java
+++ src/com/simple/Algorithms.java
@@ -77,23 +77,53 @@ public class Algorithms {
     }
 
     public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+
         char[] s1Chars = s1.toCharArray();
         int[] s1Map = new int[128];
         for (char c: s1Chars) {
             s1Map[c]++;
         }
+
         int windowLength = s1.length();
+
         char[] s2Chars = s2.toCharArray();
+        int[] s2Map = new int[128];
+        for (int i = 0; i < windowLength; i++) {
+            s2Map[s2Chars[i]]++;
+        }
 
         int left = 0;
         while ((left + windowLength) <= s2.length()) {
-            int[] s2Map = new int[128];
-            for (int i = left; i < left + windowLength; i++) {
-                s2Map[s2Chars[i]]++;
+            if (left != 0) {
+                s2Map[s2Chars[left - 1]]--;
+                s2Map[s2Chars[left + windowLength - 1]]++;
             }
             if (Arrays.equals(s1Map, s2Map)) return true;
             left++;
         }
+
         return false;
     }
+
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int originalColor = image[sr][sc];
+        if (newColor != originalColor) {
+            dfs(image, sr, sc, originalColor, newColor);
+        }
+        return image;
+    }
+
+    private void dfs(int[][] image, int sr, int sc, int color, int newColor) {
+        if (color == image[sr][sc]) {
+            image[sr][sc] = newColor;
+            if (sr - 1 >= 0) dfs(image, sr - 1, sc,color, newColor);
+            if (sc - 1 >= 0) dfs(image, sr, sc - 1,color, newColor);
+            if (sr + 1 < image[0].length) dfs(image, sr + 1, sc,color, newColor);
+            if (sc + 1 < image.length) dfs(image, sr, sc + 1,color, newColor);
+        }
+    }
+
+
 }
